7e53570dbb979b70c8aa0f0f7bc04c6634dcfcdc
Xavier de Boysson <Xavier de Boysson>
2017-02-01 03:45:59 +0100

    Cleaning up

--- Main.js
+++ Main.js
@@ -1,15 +1,3 @@
-function printCard(card)
-{
-    print(card.name);
-    print(card.power + "/" + card.life);
-    var abilities = "";
-    card.abilities.forEach((ability) =>
-    {
-        abilities = abilities + ability;
-    });
-    print(abilities);
-    
-}
 function setup()
 {
     printCard(creatures.red.retromancer);
@@ -178,10 +166,6 @@ function draw()
     requestAnimationFrame(draw);
 }
 
-function lerp(v0, v1, t)
-{
-    return (1-t)*v0 + t*v1;
-}
 
 function touched(touch)
 {
--- misc/misc.js
+++ misc/misc.js
@@ -6,6 +6,26 @@ function loadjs(filename)
 }
 
 
+function printCard(card)
+{
+    print(card.name);
+    print(card.power + "/" + card.life);
+    var abilities = "";
+    card.abilities.forEach((ability) =>
+    {
+        abilities = abilities + ability;
+    });
+    print(abilities);
+    
+}
+
+
+function lerp(v0, v1, t)
+{
+    return (1-t)*v0 + t*v1;
+}
+
+
 string = {};
 string.len = function(){};
 
