7725b5566a3ccc281dc719b6e815005311f5e712
Xavier de Boysson <Xavier de Boysson>
2017-02-01 13:57:40 +0100

    Testing phaser

--- init.js
+++ init.js
@@ -1,3 +1,27 @@
+window.onload = function()
+{
+	var game = new Phaser.Game("100", "100", Phaser.AUTO, '', { create: create, update: update });
+	
+
+	function create ()
+	{
+		game.add.text(game.world.centerX, game.world.centerY, "ok", { font: "65px Arial", fill: "#ff0044", align: "center" });
+		graphics = game.add.graphics();
+	}
+	
+	var time = 0;
+	function update()
+	{
+		time += 0;//game.time.elapsedMS;
+		
+		rect(20, 20, 100, 100);
+	}
+
+};
+
+var graphics = null;
+
+
 function init()
 {
 	setup();
--- misc/graphics.js
+++ misc/graphics.js
@@ -1,9 +1,17 @@
-function fill(){};
+var fillColor = '0xff0000';
+function fill(r, g, b, a)
+{
+    fillColor = '0x' + ("0" + r.toString(16)).slice(-2) + ("0" + g.toString(16)).slice(-2) + ("0" + b.toString(16)).slice(-2);
+}
 function ortho(){};
 function background(){};
 function text(){};
 function scale(){};
-function rect(){};
+function rect(x, y, w, h){
+	graphics.beginFill(fillColor);
+    graphics.drawRect(x, y, w, h);
+    graphics.endFill();
+};
 function translate(){};
 function popMatrix(){};
 function pushMatrix(){};
